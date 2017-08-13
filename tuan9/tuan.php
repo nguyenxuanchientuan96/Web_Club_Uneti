ac3f7ccda0a3ac583cbe1f16e3c07e8fb6b7800f
nguyenxuanchientuan96 <chientuan084@gmail.com>
2017-08-13 18:32:28 +0700

    buoi dau php

--- tuan9/tuan.php
+++ tuan9/tuan.php
@@ -1,125 +1,159 @@
 <?php
+	//php được viết trong cặp thẻ <?php 
+	// -----------------
+	
+	//Bien trong php:
 	//bien: duoc viet bang cong thuc $+tenbien
-	// $name="Nguyen Xuan Chien Tuan";
-	// echo $name;
+	$name="Nguyen Xuan Chien Tuan";
+	echo $name;
+	echo "<br/>"; //xuống hàng cho đẹp :3
+	
+	//Nối biến: Dùng dấu . để nối biến
+	$a="Nhan sắc có hạn";
+	$b="Thủ đoạn vô  biên";
+	echo $a.$b;
+	echo "<br/>"; //xuống hàng cho đẹp :3
 
-	// $a="Nhan sắc có hạn";
-	// $b="Thủ đoạn vô  biên";
-	// echo $a.$b;
 
+	// ------- hằng số
 	//hang so: la những gì k thay đổi
-	// define("tuoi", "12");
-	// echo tuoi;
+	//Công thức: define(name, value)
+	
+	define("tuoi", "12");
+	echo tuoi;
+	echo "<br/>"; //xuống hàng cho đẹp :3
 
+
+	// -------------
+	//Hàm var_dump
 	//dung var_dump để kiểm tra kiểu dữ liệu
-	// $so="123456789";
-	// var_dump($so);
-	
+	$so="123456789";
+	var_dump($so);
+	echo "<br/>"; //xuống hàng cho đẹp :3
 
+
+	// --------------
 	//mang trong php
-	// $array=array('1','2','3','4');
+	$array=array('1','2','3','4');
 
-	// echo $array[2];
-	
+	echo $array[2];
+	echo "<br/>"; //xuống hàng cho đẹp :3
+
+
+	// ----------------
 	//Toan tu trong  php
-	// $toantu= 3+3;
-	// echo $toantu;
-//==: so sanh
-//=: gan
+	$toantu= 3+3;
+	echo $toantu;
+	echo "<br/>"; //xuống hàng cho đẹp :3
+	//==: so sanh
+	//=: gan
 
-	//if else
-	// $age=19;
-	// if ($age<18) {
-	// 	echo "Vị thành niên";
-	// }
-	// elseif($age==18){
-	// 	echo "Thanh niên";
-	// }
-	// elseif($age==20){
-	// 	echo "Thanh niên +2";
-	// }
-	// elseif($age==22){
-	// 	echo "Thanh niên +4";
-	// }
-	// else{
-	// 	echo "Được xem";
-	// }
+	// ------------------
+	//if else trong php
+	$age=19;
+	if ($age<18) {
+		echo "Vị thành niên";
+	}
+	elseif($age==18){
+		echo "Thanh niên";
+	}
+	elseif($age==20){
+		echo "Thanh niên +2";
+	}
+	elseif($age==22){
+		echo "Thanh niên +4";
+	}
+	else{
+		echo "Được xem";
+	}
 
 
-	//switch case
-	// $tuoi= 18;
+	echo "<br/>"; //xuống hàng cho đẹp :3
+	
 
-	// switch ($tuoi) {
-	// 	case '18':
-	// 		echo "Thanh niên";
-	// 		break;
+	// ------------
+	// switch case : tương tự if else
+	$tuoi= 18;
 
-	// 	case '19':
-	// 		echo "Thanh niên + 1";
-	// 		break;
+	switch ($tuoi) {
+		case '18':
+			echo "Thanh niên";
+			break;
 
-	// 	case '20':
-	// 		echo "Thanh niên +2";
-	// 		break;
+		case '19':
+			echo "Thanh niên + 1";
+			break;
 
-	// 	case '21':
-	// 		echo "Thanh niên +3";
-	// 		break;
+		case '20':
+			echo "Thanh niên +2";
+			break;
+
+		case '21':
+			echo "Thanh niên +3";
+			break;
 		
-	// 	default:
-	// 		echo "cái gì đó";
-	// 		break;
-	// }
+		default:
+			echo "cái gì đó";
+			break;
+	}
+
+	echo "<br/>"; //xuống hàng cho đẹp :3
+	
 
 	// vong lap for
-	// for($i=1; $i<=10; $i++){
+	for($i=1; $i<=10; $i++){
 
-	// 	echo $i .",";
-	// }
+		echo $i .",";
+	}
+
+	echo "<br/>"; //xuống hàng cho đẹp :3
+	
 
 	//range
-	// $nam=range(1960,2017);
-	// echo $nam[30];
-	
+	$nam=range(1960,2017);
+	echo $nam[30];
+	echo "<br/>"; //xuống hàng cho đẹp :3
+
 	// ham for-each
-	// $thang=array(
-	// 	1=>"Tháng 1",
-	// 	2=>"Tháng 2",
-	// 	3=>"Tháng 3",
-	// 	4=>"Tháng 4",
-	// 	5=>"Tháng 5",
-	// 	6=>"Tháng 6",
-	// 	7=>"Tháng 7",
-	// 	8=>"Tháng 8",
-	// 	9=>"Tháng 9",
-	// 	10=>"Tháng 10",
-	// 	11=>"Tháng 11",
-	// 	12=>"Tháng 12",
-	// );
-	// foreach ($thang as $key => $value) {
-	// 	echo $value;
-	// }
+	$thang=array(
+		1=>"Tháng 1",
+		2=>"Tháng 2",
+		3=>"Tháng 3",
+		4=>"Tháng 4",
+		5=>"Tháng 5",
+		6=>"Tháng 6",
+		7=>"Tháng 7",
+		8=>"Tháng 8",
+		9=>"Tháng 9",
+		10=>"Tháng 10",
+		11=>"Tháng 11",
+		12=>"Tháng 12",
+	);
+	foreach ($thang as $key => $value) {
+		echo $value;
+	}
 
+	echo "<br/>"; //xuống hàng cho đẹp :3
 	//while
-	// $i = 100;
-
-	// while ($i <= 200){
-	//     echo $i;
-	//   $i++;
-	// }
+	$i = 100;
+	while ($i <= 200){
+	    echo $i;
+	  $i++;
+	}
+	echo "<br/>"; //xuống hàng cho đẹp :3
 
 
 //hàm trong php  
 
-// //ham cong 2 so
-// 	function tinhtong($a, $b, $c, $d, $e )
-// 	{
-// 	    return $a + $b - $c + $d -$e;
-// 	}
+//ham cong tru nhieu so
+	function tinhtong($a, $b, $c, $d, $e )
+	{
+	    return $a + $b - $c + $d -$e;
+	}
 
 
-// 	$tong = tinhtong(30,40,30,30,30);
-// 	echo $tong;
+	$tong = tinhtong(30,40,30,30,30);
+	echo $tong;
 	
 	
 
