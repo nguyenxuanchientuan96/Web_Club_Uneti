b752158f759e02a0a9f7aceef2cc2f6343f55662
nguyenxuanchientuan96 <chientuan084@gmail.com>
2017-08-13 18:18:30 +0700

    Buoi dau tien PHP

---
+++ tuan9/tuan.php
@@ -0,0 +1,127 @@
+<?php
+	//bien: duoc viet bang cong thuc $+tenbien
+	// $name="Nguyen Xuan Chien Tuan";
+	// echo $name;
+
+	// $a="Nhan sắc có hạn";
+	// $b="Thủ đoạn vô  biên";
+	// echo $a.$b;
+
+	//hang so: la những gì k thay đổi
+	// define("tuoi", "12");
+	// echo tuoi;
+
+	//dung var_dump để kiểm tra kiểu dữ liệu
+	// $so="123456789";
+	// var_dump($so);
+	
+
+	//mang trong php
+	// $array=array('1','2','3','4');
+
+	// echo $array[2];
+	
+	//Toan tu trong  php
+	// $toantu= 3+3;
+	// echo $toantu;
+//==: so sanh
+//=: gan
+
+	//if else
+	// $age=19;
+	// if ($age<18) {
+	// 	echo "Vị thành niên";
+	// }
+	// elseif($age==18){
+	// 	echo "Thanh niên";
+	// }
+	// elseif($age==20){
+	// 	echo "Thanh niên +2";
+	// }
+	// elseif($age==22){
+	// 	echo "Thanh niên +4";
+	// }
+	// else{
+	// 	echo "Được xem";
+	// }
+
+
+	//switch case
+	// $tuoi= 18;
+
+	// switch ($tuoi) {
+	// 	case '18':
+	// 		echo "Thanh niên";
+	// 		break;
+
+	// 	case '19':
+	// 		echo "Thanh niên + 1";
+	// 		break;
+
+	// 	case '20':
+	// 		echo "Thanh niên +2";
+	// 		break;
+
+	// 	case '21':
+	// 		echo "Thanh niên +3";
+	// 		break;
+		
+	// 	default:
+	// 		echo "cái gì đó";
+	// 		break;
+	// }
+
+	// vong lap for
+	// for($i=1; $i<=10; $i++){
+
+	// 	echo $i .",";
+	// }
+
+	//range
+	// $nam=range(1960,2017);
+	// echo $nam[30];
+	
+	// ham for-each
+	// $thang=array(
+	// 	1=>"Tháng 1",
+	// 	2=>"Tháng 2",
+	// 	3=>"Tháng 3",
+	// 	4=>"Tháng 4",
+	// 	5=>"Tháng 5",
+	// 	6=>"Tháng 6",
+	// 	7=>"Tháng 7",
+	// 	8=>"Tháng 8",
+	// 	9=>"Tháng 9",
+	// 	10=>"Tháng 10",
+	// 	11=>"Tháng 11",
+	// 	12=>"Tháng 12",
+	// );
+	// foreach ($thang as $key => $value) {
+	// 	echo $value;
+	// }
+
+	//while
+	// $i = 100;
+
+	// while ($i <= 200){
+	//     echo $i;
+	//   $i++;
+	// }
+
+
+//hàm trong php  
+
+// //ham cong 2 so
+// 	function tinhtong($a, $b, $c, $d, $e )
+// 	{
+// 	    return $a + $b - $c + $d -$e;
+// 	}
+
+
+// 	$tong = tinhtong(30,40,30,30,30);
+// 	echo $tong;
+	
+	
+
+
+?>
